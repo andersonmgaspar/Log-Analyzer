f0360244149b4f42b90032ca85fe7362331b8817
Andi <andersonmgaspar@gmail.com>
2019-07-15 22:46:28 -0300

    udacity review changes

--- log_report.py
+++ log_report.py
@@ -10,12 +10,12 @@ top3_articles = """select title, count(log.id) as views
                    group by articles.title
                    order by views DESC LIMIT 3;"""
 
-popular_authors =  """select authors.name, count(log.id) as views
-                      from articles, authors, log
-                      where articles.slug = substring(log.path, 10)
-                      and authors.id = articles.author
-                      group by authors.name
-                      order by views DESC;"""
+popular_authors = """select authors.name, count(log.id) as views
+                     from articles, authors, log
+                     where articles.slug = substring(log.path, 10)
+                     and authors.id = articles.author
+                     group by authors.name
+                     order by views DESC;"""
 
 # View to show all errors by date
 """
@@ -34,7 +34,8 @@ FROM log
 GROUP BY date;
 """
 
-error_count = """SELECT errorcount.date, round(100.0*totalerrors/totalcount,2) as percent
+error_count = """SELECT errorcount.date, round(100.0*totalerrors/totalcount,2)
+                 as percent
                  FROM errorcount, logcount
                  WHERE logcount.date = errorcount.date
                  AND totalerrors > totalcount/100; """
@@ -48,26 +49,26 @@ def make_query(query):
         cursor.execute(query)
         posts = cursor.fetchall()
         return posts
-    except (Exception, psycopg2.Error) as error :
+    except (Exception, psycopg2.Error) as error:
         print ("Error while fetching data from PostgreSQL", error)
     finally:
-        #closing database connection.
         if(connection):
             cursor.close()
             connection.close()
 
-# Show Popular Authors
 def show_results(query_statement, query):
-        results = make_query(query)
-        print("");
-        print(query_statement)
-        for row in results:
-           author = str(row[0])
-           result_views = str(row[1])
-           print(author + " - " + result_views)
-        print("");
+    results = make_query(query)
+    print("")
+    print(query_statement)
+    for row in results:
+        author = str(row[0])
+        result_views = str(row[1])
+        print(author + " with " + result_views + " views.")
+    print("")
 
-
-show_results("Top 3 Articles: ", top3_articles)
-show_results("Popular Authors: ", popular_authors)
-show_results("Dates with access problems: ", error_count)
+show_results("What are the most popular three articles of all time? ", top3_articles)
+show_results("Who are the most popular article authors of all time? ", popular_authors)
+errors = make_query(error_count)
+print("On which days more than 1% of the requests led to error?")
+print(str(errors[0][0]) + " - " + str(errors[0][1]) + "%")
+#show_results("Dates with access problems: ", error_count)
